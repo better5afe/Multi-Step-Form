d6ed96cb77b7bc820dade079dddbf291e11737f4
better5afe <abramczyk2704@gmail.com>
2023-10-11 16:58:37 +0200

    Implemented main app body for desktop viewports

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import FormNavigation from './components/navigation/FormNavigation';
 
 const App = () => {
 	return (
-		<main className='font-sans text-sm large:text-base text-coolGray'>
+		<main className='font-sans text-sm large:text-base text-coolGray large:w-[950px] large:p-5 large:bg-alabaster large:rounded-xl large:shadow-lg large:shadow-lightGray'>
 			<FormNavigation />
 		</main>
 	);
